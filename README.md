5090898d10fa72a07e23b093fbef2d078381d6ae
jptonelli <joaopedrotonelli9@gmail.com>
2023-07-10 12:10:15 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Landing-Page-iPhone
+ Nesse projeto foi criado uma página para o novo iPhone 13 no site da Apple. Foi utilizado HTML, CSS e Javascript
---
+++ index.js
@@ -0,0 +1,8 @@
+function trocarCor(e){
+    document.querySelector('#phone').src = e;
+}
+
+function trocarCorCírculo(color){
+    const circle = document.querySelector('.circle')
+    circle.style.background = color
+}
